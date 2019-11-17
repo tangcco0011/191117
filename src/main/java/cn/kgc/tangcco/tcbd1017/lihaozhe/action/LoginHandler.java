97a193bb3cb0aabda32646d37fb98d11b6765fb3
李昊哲 <646269678@qq.com>
2019-11-17 15:04:25 +0800

    控制层保存用户功能编写完成

--- src/main/java/cn/kgc/tangcco/tcbd1017/lihaozhe/action/LoginHandler.java
+++ src/main/java/cn/kgc/tangcco/tcbd1017/lihaozhe/action/LoginHandler.java
@@ -7,14 +7,19 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import cn.kgc.tangcco.tcbd1017.lihaozhe.service.UserService;
+import cn.kgc.tangcco.tcbd1017.lihaozhe.service.impl.UserServiceImpl;
+
 /**
  * Servlet implementation class LoginHandler
  */
-@WebServlet("/LoginHandler")
+@WebServlet("/login.action")
 public class LoginHandler extends HttpServlet {
-	private static final long serialVersionUID = 1L;
        
-    /**
+	private static final long serialVersionUID = -7745510595361767547L;
+	private UserService userService = new UserServiceImpl();
+
+	/**
      * @see HttpServlet#HttpServlet()
      */
     public LoginHandler() {
@@ -25,7 +30,7 @@ public class LoginHandler extends HttpServlet {
 	 * @see HttpServlet#service(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		
+		userService.saveUser();
 	}
 
 }
